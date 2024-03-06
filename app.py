2d3533d23a9384db84d9c2359ba8993ff77c157d
MD TAHSEEN RAZA <114172906+mdtahseenraza@users.noreply.github.com>
2024-03-07 03:48:54 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+
+from dotenv import load_dotenv
+
+load_dotenv()  
+
+import streamlit as st
+import os
+import pathlib
+import textwrap
+
+import google.generativeai as genai
+
+from IPython.display import display
+from IPython.display import Markdown
+
+
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_gemini_response(question):
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(question)
+    return response.text
+
+
+st.set_page_config(page_title="Q&A Demo")
+
+st.header("Gemini Application")
+
+input=st.text_input("Input: ",key="input")
+
+
+submit=st.button("Ask the question")
+
+
+if submit:
+    
+    response=get_gemini_response(input)
+    st.subheader("The Response is")
+    st.write(response)
