ebc943378f163334efb3ae370a57576a42856ff2
MD TAHSEEN RAZA <114172906+mdtahseenraza@users.noreply.github.com>
2024-03-08 21:50:42 +0530

    Update project.py

--- project.py
+++ project.py
@@ -23,7 +23,7 @@ page_bg_img = f"""
 /* Set background image for the main content area */
 [data-testid="stAppViewContainer"] > .main {{
     background-image: url("https://images.unsplash.com/photo-1618367588411-d9a90fefa881?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
-    background-size: 125%;
+    background-size: 180%;
     background-attachment: local;
 }}
 
